a7e8dc4c955f47de8225e2557d9fe2ed861ecad1
Arland Gamble <agdigital2030@gmail.com>
2024-01-30 01:53:50 -0500

    first

---
+++ test.js
@@ -0,0 +1,10 @@
+function getAverage. (a,b){
+
+     var avervage = (a + b) / 2; 
+    console.log(average);
+    return average;
+
+
+}
+var myResult = getAverage(7,8);
+console.log("the average is " + myResut);
